e5b6647a298c960b64f5fbb9ca6698dde0f6a67a
Lucas Menezes <luckmenez@gmail.com>
2023-02-01 19:00:54 -0300

    feat: createUser use case test implementation

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,39 @@
+import { AppError } from "../../../../shared/errors/AppError";
+import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "./CreateUserUseCase"
+
+let createUserUseCase: CreateUserUseCase;
+let userRepositoryInMemory: InMemoryUsersRepository;
+describe("Create User", () => {
+  beforeEach(() => {
+    userRepositoryInMemory = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(userRepositoryInMemory);
+  })
+
+  it("should be able to create a new user", async () => {
+    const user = await createUserUseCase.execute({
+      email:"test@email.com.br",
+      name:"Test",
+      password:"12345"
+    })
+
+    expect(user).not.toBe(undefined)
+    expect(user).toHaveProperty("id")
+  })
+
+  it("should not be able to crete the same user twice", () => {
+    expect(async () => {
+      const user = await createUserUseCase.execute({
+        email:"test@email.com.br",
+        name:"Test",
+        password:"12345"
+      })
+
+      const duplicatedUser = await createUserUseCase.execute({
+        email:"test@email.com.br",
+        name:"Test",
+        password:"12345"
+      })
+    }).rejects.toBeInstanceOf(AppError)
+  })
+})
