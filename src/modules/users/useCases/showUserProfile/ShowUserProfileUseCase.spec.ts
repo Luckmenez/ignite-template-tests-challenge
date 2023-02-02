d2783ac336bb501df6671b8b71ef0109f71a51d9
Lucas Menezes <luckmenez@gmail.com>
2023-02-01 22:26:15 -0300

    feat: ShowUserProfile Use case test implementation

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -0,0 +1,29 @@
+import { User } from "../../entities/User";
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
+
+let showUserProfileUseCase: ShowUserProfileUseCase;
+let createUserUseCase: CreateUserUseCase;
+let inMemoryUsersRepository: InMemoryUsersRepository;
+
+describe("User Profile", () => {
+  beforeAll(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository)
+    showUserProfileUseCase = new ShowUserProfileUseCase(inMemoryUsersRepository)
+  })
+
+  it("should be able to show users profile", async () => {
+    const user = await createUserUseCase.execute({
+      email:"test@test.com.br",
+      password:"test",
+      name:"test",
+    })
+
+    const userProfile = await showUserProfileUseCase.execute(user.id as string)
+
+    expect(userProfile).toHaveProperty("id")
+    expect(userProfile).toBeInstanceOf(User)
+  })
+})
