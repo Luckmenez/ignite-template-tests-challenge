fca6132a61476b1d33839ed46dd100b1b7718ede
Lucas Menezes <luckmenez@gmail.com>
2023-02-06 14:48:59 -0300

    feat: add CreateUserController  integration test

---
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -0,0 +1,45 @@
+import request from "supertest";
+import { Connection } from "typeorm";
+import { app } from "../../../../app";
+import createConnection from "../../../../database";
+
+let connection: Connection;
+describe("Create User Controller", () => {
+
+  beforeAll(async() => {
+    connection = await createConnection();
+    await connection.runMigrations()
+  })
+
+  afterAll(async() => {
+    await connection.dropDatabase()
+    await connection.close()
+  })
+
+  it("should be able to create a new user", async () => {
+    const response = await request(app).post('/api/v1/users').send({
+      name: "SuperTest",
+      email: "supertest@supertest.com.br",
+      password: "supertest"
+    })
+
+    expect(response.status).toBe(201);
+  })
+
+  it("should not be able to create the same user twice", async () => {
+    const responseUser = await request(app).post('/api/v1/users').send({
+      name: "SuperTest",
+      email: "supertest@supertest.com.br",
+      password: "supertest"
+    })
+
+    const responseRepeatedUser = await request(app).post('/api/v1/users').send({
+      name: "SuperTest",
+      email: "supertest@supertest.com.br",
+      password: "supertest"
+    })
+
+    expect(responseRepeatedUser.status).toBe(400);
+    expect(responseRepeatedUser.body.message).toBe('User already exists');
+  })
+})
